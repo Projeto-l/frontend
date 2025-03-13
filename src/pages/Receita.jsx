9eac340f1ae526fa9d45b87c8bffacfd0fb4e0b5
alexmof <alex.fidelis@ccc.ufcg.edu.br>
2025-03-12 22:09:25 -0300

    feat: adding header

--- src/pages/Receita.jsx
+++ src/pages/Receita.jsx
@@ -1,8 +1,10 @@
+import { Header } from "../components/Header.jsx";
 import { ReceitaCard } from "../components/ReceitaCard.jsx";
 
 const Receita = () => {
   return (
     <div>
+      <Header isHome={false}></Header>
       <ReceitaCard />
     </div>
   );
