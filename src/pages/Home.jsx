41635f7eec966fd83b430c63ad56e602253969bf
alexmof <alex.fidelis@ccc.ufcg.edu.br>
2025-03-14 12:59:41 -0300

    feat: adding Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import '../styles/Home.css'
+import { Link, useNavigate } from "react-router-dom";
+import { Icon } from "../components/Icons.jsx";
 import { Header } from "../components/Header.jsx";
+import { RecentCard } from "../components/RecentCard.jsx";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Página Inicial</h1>
-      <Header isHome={true}></Header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Registrar</Link>
-          </li>
-          <li>
-            <Link to="/receita">Receita</Link>
-          </li>
-          <li>
-            <Link to="/calculator">Calculator</Link> 
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+
+    const navigate = useNavigate();
+
+    const navigateTo = (path) =>{
+        console.log(path);
+        navigate(path);
+    }
+
+    return (
+        <div className="home">
+            <Header isHome={true}></Header>
+            <div className="menu-card">
+                <h2 className="menu-card-title">
+                    Menu
+                </h2>
+                <div className="menu-buttons-container">
+                    <button className="menu-button" onClick={() => {navigateTo("/calculator")}} >
+                        <Icon name="calculator"></Icon>
+                        <span>Calculadora</span>
+                    </button>
+                    <button className="menu-button" onClick={() => {navigateTo("/prescription")}} >
+                        <Icon name="prescription"></Icon>
+                        <span>Nova Receita</span>
+                    </button>
+                    <button className="menu-button" onClick={() => {navigateTo("/prescriptions")}} >
+                        <Icon name="prescriptions"></Icon>
+                        <span>Receitas</span>
+                    </button>
+                </div>
+            </div>
+            <RecentCard />
+        </div>
+    );
 };
 
 export default Home;
